90ecdcb58b7004fb6149b0462246a12b0743538c
wengshuang <200953091@qq.com>
2024-07-11 16:59:45 +0800

    feat: 添加403和404页面

---
+++ src/routes/403.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Result } from 'antd';
+
+const App: React.FC = () => <Result status="403" title="403" />;
+
+export default App;
---
+++ src/routes/404.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Result } from 'antd';
+
+const App: React.FC = () => <Result status="404" title="404" />;
+
+export default App;
